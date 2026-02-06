63fee3a75b76b0f59e755b85d7b3dafe238dd30a
Bhanush03 <bhanushkarpady@gmail.com>
2026-02-06 12:00:37 +0530

    Add files via upload

---
+++ day3/day3task3.py
@@ -0,0 +1,9 @@
+products = ["Milk", "Bread", "Rice", "Sugar"]
+
+print("Store Stock:", products)
+
+products.append("Oil")
+products.remove("Bread")
+products.sort()
+
+print("Updated Stock:", products)
